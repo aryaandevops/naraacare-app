a98831aaa95b249203c95c0fe91f59a7a9fd6e48
aryaandevops <aryaandhole@gmail.com>
2026-09-16 00:10:44 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,17 @@
-```dart
 import 'package:flutter_test/flutter_test.dart';
 import 'package:naara_care_app/main.dart';
 
 void main() {
-  testWidgets('app launches to splash screen', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+testWidgets('app launches to splash screen', (WidgetTester tester) async {
+await tester.pumpWidget(const MyApp());
 
-    // Render the initial splash screen.
-    await tester.pump();
-
-    // Let the 4-second splash timer complete.
-    await tester.pump(const Duration(seconds: 4));
-
-    // Process the navigation/rebuild.
-    await tester.pump();
-  });
-}
 ```
+await tester.pump();
+
+await tester.pump(const Duration(seconds: 4));
+
+await tester.pump();
+```
+
+});
+}
